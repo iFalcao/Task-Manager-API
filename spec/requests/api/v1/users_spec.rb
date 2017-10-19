47e52e1a29c116b2fd503f987910a4248a6ec60e
Icaro Falcao <vbsfalcao@gmail.com>
2017-10-19 09:02:38 -0300

    Fix request headers of rspec user request testing + remove presence validation of name in users

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -4,17 +4,17 @@ RSpec.describe 'Users API', type: :request do
   let!(:user) { create(:user) }
   let(:user_id) { user.id }
 
-  describe "Get /users/:id" do
+  describe "GET /users/:id" do
     before do 
       headers = { "Accept" => "application/vnd.taskmanager.v1" }
-      # get path, body, headers
-      get "/users/#{user_id}", {}, headers
+      get "/users/#{user_id}"
+      request.headers.merge!(headers)
     end
 
     context "When the user exists" do
       it "returns the user" do
         user_response = JSON.parse(response.body)
-        expect(user_response[:id]).to eq(user_id)
+        expect(user_response["id"]).to eq(user_id)
       end
     end
 
