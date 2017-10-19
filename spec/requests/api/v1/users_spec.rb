cca8556808b0fcf6e3e953bc97978fb14bace2e9
Icaro Falcao <vbsfalcao@gmail.com>
2017-10-19 09:21:40 -0300

    Fix subdomain name in request tests

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -3,12 +3,13 @@ require 'rails_helper'
 RSpec.describe 'Users API', type: :request do
   let!(:user) { create(:user) }
   let(:user_id) { user.id }
+  # Fix API subdomain
+  before { host! "api.taskmanager.dev" }
 
   describe "GET /users/:id" do
     before do 
       headers = { "Accept" => "application/vnd.taskmanager.v1" }
-      get "/users/#{user_id}"
-      request.headers.merge!(headers)
+      get "/users/#{user_id}", params: {}, headers: headers
     end
 
     context "When the user exists" do
