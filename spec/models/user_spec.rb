195164b0ad656040933a20221b836f5d4496179b
Icaro Falcao <vbsfalcao@gmail.com>
2017-10-19 08:04:42 -0300

    Use testing best practices of RSpec and ShouldaMatchers + create virtual attribute :name in User + configure rspec to test in random order

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before { @user = FactoryGirl.build :user }
+  let(:user) { build :user }
 
-  it { expect(@user).to respond_to :email }
-  it { expect(@user).to respond_to :password } 
-  it { expect(@user).to respond_to :password_confirmation }
-  it { expect(@user).to be_valid }  
+  # If the test doesn't need a build object, you can use:
+  it { is_expected.to validate_presence_of(:email) }
+
+  # Working with Shoulda Matchers methods to create less verbose tests
+  it { expect(user).to validate_presence_of(:name) }  
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+  it { is_expected.to allow_value("user@test.com").for(:email) }
+  it { is_expected.to validate_confirmation_of(:password) }
+
+=begin
+  subject { FactoryGirl.build :user }
+  it { expect(subject).to respond_to :email }
+  
+  context 'when user name is blank' do
+    before { user.name = nil }
+
+    it { expect(user).not_to be_valid }
+  end
+=end
+
 end
