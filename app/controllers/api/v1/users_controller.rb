d89aadfecfad96984f1db9bd27a46b397c22eccc
Icaro Falcao <vbsfalcao@gmail.com>
2017-10-20 10:28:30 -0300

    Create controller method to handle user creation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,11 +3,27 @@ class Api::V1::UsersController < ApplicationController
 
   def show
     begin
-      @user = User.find params[:id]
-      respond_with @user
+      user = User.find(params[:id])
+      respond_with user
     rescue => exception
       head 404
     end
   end
+
+  def create
+    user = User.new(params_user)
+
+    if user.save
+      render json: user, status: 201
+    else
+      
+    end
+  end
+
+  protected 
+
+    def params_user
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 
 end
