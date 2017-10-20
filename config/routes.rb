c1e2319186758467c0954ad0c7f5437873f27cc9
Icaro Falcao <vbsfalcao@gmail.com>
2017-10-20 09:57:05 -0300

    Disable devise routes temporarily

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ require 'api_version_constraint'
 
 Rails.application.routes.draw do
 
-  devise_for :users
+  # devise_for :users
   # Makes the api accessible through the domain -> api.site.com/resource-name
+  
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
     # Extra configuration about the API should be passed on the headers instead of URL
     namespace :v1, path: '/', constraints: ApiVersionConstraint.new( version: 1, default: true ) do
