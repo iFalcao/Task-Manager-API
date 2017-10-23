c1fc97ef161979dba7def7704075a15491455d88
Icaro Falcao <vbsfalcao@gmail.com>
2017-10-23 20:37:24 -0300

    Uses devise helper to users log in and generate new authentication token + returns json with errors in case of invalid credentials

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,7 +4,14 @@ class Api::V1::SessionsController < ApplicationController
     user = User.find_by(email: params_session[:email])
 
     if user && user.valid_password?(params_session[:password])
+      # 'Sign in' is a Devise Helper to record login info to database
+      # and store: false is to not store the session since the API is stateless
+      sign_in user, store: false
+      user.generate_authentication_token!
+      user.save
       render json: user, status: 200
+    else
+      render json: { errors: 'Invalid password or e-mail' }, status: 401
     end
   end
 
