b14b51a52bf4322d530ed86dcbbc12374c290937
Icaro Falcao <vbsfalcao@gmail.com>
2017-10-24 22:43:43 -0300

    Create action index with tests for model Task

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,2 +1,9 @@
-class TasksController < ApplicationController
+class Api::V1::TasksController < ApplicationController
+  before_action :authenticate_with_token!
+
+  def index
+    tasks = current_user.tasks
+    render json: { tasks: tasks }, status: 200
+  end
+
 end
--- spec/requests/api/v1/tasks_spec.rb
+++ spec/requests/api/v1/tasks_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
 
-RSpec.describe TasksController, type: :request do
+RSpec.describe 'Tasks API', type: :request do
+  before { host! 'api.taskmanager.dev' }
+  let!(:user) { create(:user) }
+  let!(:task) { create(:task) }
+  
+  let(:headers) do
+    {
+      'Accept' => 'application/vnd.taskmanager.v1',
+      'Content-Type' => Mime[:json].to_s,
+      'Authorization' => user.auth_token
+    }
+  end
 
+  describe 'GET /tasks' do
+    before do
+      create_list(:task, 5, user_id: user.id)
+      get "/tasks", params: {}, headers: headers
+    end
+
+    it 'return status 200' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'return 5 tasks' do
+      expect(json_body[:tasks].count).to eq(5)
+    end
+  end
+
+
+=begin
+  describe 'GET /tasks/:id' do
+    before do
+      get "/tasks/#{task.id}", params: {}, headers: headers
+    end
+
+    context 'when the task exists' do
+      it 'return status 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'return json data with the task' do
+        expect(json_body[:id]).to eq(task.id)
+      end
+    end
+
+    context 'when the task does not exist' do
+      
+    end
+  end
+=end
 end
