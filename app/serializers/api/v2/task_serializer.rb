ca64d7127d014329183dc00549106f9ba963a8f3
Icaro Falcao <vbsfalcao@gmail.com>
2017-11-21 10:43:11 -0300

    Add Serializer to Task in v2

---
+++ app/serializers/api/v2/task_serializer.rb
@@ -0,0 +1,15 @@
+class Api::V2::TaskSerializer < ActiveModel::Serializer
+  attributes :id, :title, :description, :done, :user_id, 
+             :deadline, :created_at, :updated_at, 
+             :short_description, :is_late
+
+  def short_description
+    object.description[0..40]
+  end
+
+  def is_late
+    Time.current > object.deadline if object.deadline.present?
+  end
+
+  belongs_to :user
+end
