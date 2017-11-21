fbac95b8af9ad6cf7d9b0594096e0e74fc522485
Icaro Falcao <vbsfalcao@gmail.com>
2017-11-21 10:43:53 -0300

    Follow json_api best practices by adding its adapter and fixes new tests in v2

--- spec/requests/api/v2/tasks_spec.rb
+++ spec/requests/api/v2/tasks_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe 'Tasks API', type: :request do
     end
 
     it 'return 5 tasks' do
-      expect(json_body[:tasks].count).to eq(5)
+      expect(json_body[:data].count).to eq(5)
     end
   end
 
@@ -40,7 +40,7 @@ RSpec.describe 'Tasks API', type: :request do
       end
 
       it 'return json data with the task' do
-        expect(json_body[:title]).to eq(task.title)
+        expect(json_body[:data][:attributes][:title]).to eq(task.title)
       end
     end
 
@@ -70,11 +70,11 @@ RSpec.describe 'Tasks API', type: :request do
       end
 
       it 'return json with the created task' do
-        expect(json_body[:title]).to eq(task_params[:title])
+        expect(json_body[:data][:attributes][:title]).to eq(task_params[:title])
       end
 
       it 'assigns the created task to the current user' do
-        expect(json_body[:user_id]).to eq(user.id)
+        expect(json_body[:data][:attributes][:'user-id']).to eq(user.id)
       end
     end
 
@@ -112,7 +112,7 @@ RSpec.describe 'Tasks API', type: :request do
       end
 
       it 'return the updated object' do
-        expect(json_body[:title]).to eq(task_params[:title])
+        expect(json_body[:data][:attributes][:title]).to eq(task_params[:title])
       end
 
       it 'update the task in the database' do
