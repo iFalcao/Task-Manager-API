c7081b24ae3e7862e0353e48d8ed8f3aae33f069
iFalcao <vbsfalcao@gmail.com>
2019-05-19 00:53:51 -0300

    Allows creation of user with a name and update of user's name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+    def configure_permitted_parameters
+      [:sign_up, :account_update].each do |devise_action|
+        devise_parameter_sanitizer.permit(devise_action, keys: [:name])
+      end
+    end
 end
